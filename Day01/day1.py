baf03732e0a8f274329ca36c57175e186e7dcb55
David Betteridge <davidabetteridge@gmail.com>
2022-01-11 18:01:12 +0000

    Day 1 - Part 2

--- Day01/day1.py
+++ Day01/day1.py
@@ -12,19 +12,46 @@ direction = 0
 x = 0
 y = 0
 
+visited = set()
+
 for command in commands:
   if command[0] == "R":
     direction = (direction + 1) % 4
   else:
     direction = (direction - 1) % 4
 
-  if direction == 0:
-    y += int(command[1:])
-  elif direction == 2:
-    y -= int(command[1:])    
-  elif direction == 1:
-    x += int(command[1:])    
-  elif direction == 3:
-    x -= int(command[1:])        
+  distance = int(command[1:])
 
-print(abs(x)+abs(y))  #291
+  if direction == 0:
+    for _ in range(distance):
+      y += 1
+      if (x,y) in visited:
+        print(abs(x)+abs(y))
+      else:
+        visited.add((x,y))
+
+  elif direction == 2:
+    for _ in range(distance):
+      y -= 1
+      if (x,y) in visited:
+        print(abs(x)+abs(y))
+      else:
+        visited.add((x,y))
+
+  elif direction == 1:
+    for _ in range(distance):
+      x += 1
+      if (x,y) in visited:
+        print(abs(x)+abs(y))
+      else:
+        visited.add((x,y))
+
+  elif direction == 3:
+    for _ in range(distance):
+      x -= 1
+      if (x,y) in visited:
+        print(abs(x)+abs(y))
+      else:
+        visited.add((x,y))       
+
+
