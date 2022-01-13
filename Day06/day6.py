243cae102796e4a8c9aadd70a66b9933ba025dcc
David Betteridge <davidabetteridge@gmail.com>
2022-01-13 08:47:27 +0000

    Day6 - Part 2

--- Day06/day6.py
+++ Day06/day6.py
@@ -4,11 +4,14 @@ with open('Day06/data.txt') as f:
   signals = [line.strip() for line in f.readlines()]
 
 
-result = ""
+part1 = ""
+part2 = ""
 for column_number in range(len(signals[0])):
   column = []
   for signal in signals:
     column.append(signal[column_number])
   counts = Counter(column)
-  result += counts.most_common(1)[0][0]
-print(result)
+  part1 += counts.most_common(1)[0][0]
+  part2 += counts.most_common()[-1][0]
+print(part1)
+print(part2)
