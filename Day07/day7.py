da8a6da1a5e996e1c2e0c430bfc5aa3a1247e2f5
David Betteridge <davidabetteridge@gmail.com>
2022-01-13 17:47:17 +0000

    Day7 - Part 1

--- Day07/day7.py
+++ Day07/day7.py
@@ -1,5 +1,7 @@
 from typing import List, Tuple
 
+with open('Day07/data.txt') as f:
+  addresses = [line.strip() for line in f.readlines()]
 
 def split_address(address: str) -> Tuple[List[str], List[str]]:
   outsides = []
@@ -18,6 +20,18 @@ def split_address(address: str) -> Tuple[List[str], List[str]]:
 
   return insides, outsides
 
+def contains_abba(section: str) -> bool:
+  for i in range(len(section)-3):
+    if section[i] == section[i+3] and section[i+1] == section[i+2] and section[i] != section[i+1]:
+      return True
+  return False
 
-print(split_address("abba[mnop]qrst"))    
-print(split_address("abba[mnop]qrst[a]"))    
+def list_contains_abba(list: List[str]) -> bool:
+  return any(contains_abba(section) for section in list)
+
+
+def address_is_valid(address: str) -> bool:
+  insides, outsides = split_address(address)    
+  return list_contains_abba(outsides) and not list_contains_abba(insides)
+
+print(len([address for address in addresses if address_is_valid(address)]))  #118
