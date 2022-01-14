c0844de4f2f0a7b30fc5a54673a13e3d287ea93e
David Betteridge <davidabetteridge@gmail.com>
2022-01-14 08:45:50 +0000

    Day 9 - Part 2

--- Day09/day9.py
+++ Day09/day9.py
@@ -24,7 +24,7 @@ def decompressed_length(text: str) -> int:
       if number_of_chrs is not None and text[i2] == "x":
         i2, repeats = read_int(text, i2+1)
         if repeats is not None and text[i2] == ")":
-          length += number_of_chrs * repeats
+          length += repeats * decompressed_length(text[i2+1:i2+1+number_of_chrs])
           i = i2+number_of_chrs+1
           continue
     length+=1
@@ -32,11 +32,15 @@ def decompressed_length(text: str) -> int:
   return length
 
 
-assert decompressed_length("ADVENT") == 6
-assert decompressed_length("A(1x5)BC") == 7
-assert decompressed_length("(3x3)XYZ") == 9
-assert decompressed_length("A(2x2)BCD(2x2)EFG") == 11
-assert decompressed_length("(6x1)(1x3)A") == 6
-assert decompressed_length("X(8x2)(3x3)ABCY") == 18
+# assert decompressed_length("ADVENT", len) == 6
+# assert decompressed_length("A(1x5)BC", len) == 7
+# assert decompressed_length("(3x3)XYZ", len) == 9
+# assert decompressed_length("A(2x2)BCD(2x2)EFG", len) == 11
+# assert decompressed_length("(6x1)(1x3)A", len) == 6
+# assert decompressed_length("X(8x2)(3x3)ABCY", len) == 18
 
-print(decompressed_length(text))
+# print(decompressed_length(text, len))  #150914
+
+
+assert decompressed_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") == 445
+print(decompressed_length(text))  #11052855125
