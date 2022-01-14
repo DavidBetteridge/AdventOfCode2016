bf351f94cae25ffc0b2f7c8ac85ed0fdfcb02831
David Betteridge <davidabetteridge@gmail.com>
2022-01-14 08:50:10 +0000

    Combine parts

--- Day09/day9.py
+++ Day09/day9.py
@@ -15,7 +15,7 @@ def read_int(text: str, start_from: int) -> Tuple[int, Optional[int]]:
     return i, None
 
 
-def decompressed_length(text: str) -> int:
+def decompressed_length(text: str, fn) -> int:
   i = 0
   length = 0
   while i < len(text):
@@ -24,7 +24,7 @@ def decompressed_length(text: str) -> int:
       if number_of_chrs is not None and text[i2] == "x":
         i2, repeats = read_int(text, i2+1)
         if repeats is not None and text[i2] == ")":
-          length += repeats * decompressed_length(text[i2+1:i2+1+number_of_chrs])
+          length += repeats * fn(text[i2+1:i2+1+number_of_chrs], fn)
           i = i2+number_of_chrs+1
           continue
     length+=1
@@ -32,15 +32,19 @@ def decompressed_length(text: str) -> int:
   return length
 
 
-# assert decompressed_length("ADVENT", len) == 6
-# assert decompressed_length("A(1x5)BC", len) == 7
-# assert decompressed_length("(3x3)XYZ", len) == 9
-# assert decompressed_length("A(2x2)BCD(2x2)EFG", len) == 11
-# assert decompressed_length("(6x1)(1x3)A", len) == 6
-# assert decompressed_length("X(8x2)(3x3)ABCY", len) == 18
+# Part 1
+def string_len(text: str, fn) -> int:
+  return len(text)
+  
+assert decompressed_length("ADVENT", string_len) == 6
+assert decompressed_length("A(1x5)BC", string_len) == 7
+assert decompressed_length("(3x3)XYZ", string_len) == 9
+assert decompressed_length("A(2x2)BCD(2x2)EFG", string_len) == 11
+assert decompressed_length("(6x1)(1x3)A", string_len) == 6
+assert decompressed_length("X(8x2)(3x3)ABCY", string_len) == 18
+print(decompressed_length(text, string_len))  #150914
 
-# print(decompressed_length(text, len))  #150914
 
-
-assert decompressed_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN") == 445
-print(decompressed_length(text))  #11052855125
+# Part 2
+assert decompressed_length("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", decompressed_length) == 445
+print(decompressed_length(text, decompressed_length))  #11052855125
