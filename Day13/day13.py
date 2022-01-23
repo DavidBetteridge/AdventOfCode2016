aa00dc81d5c45b17aa6fdd3e662a811b4be9a47e
David Betteridge <davidabetteridge@gmail.com>
2022-01-23 21:29:05 +0000

    Day 13 Part 1

---
+++ Day13/day13.py
@@ -0,0 +1,45 @@
+import networkx as nx
+
+def what(x, y, n):
+  t = x*x + 3*x + 2*x*y + y + y*y
+  t += n
+  ones = bin(t).count("1")
+  if ones % 2 == 1:
+    return "#"
+  else:
+    return "."
+
+
+G = nx.DiGraph()
+
+n = 1352
+target_x = 31
+target_y = 39
+
+directions = [(0,-1), (-1, 0), (1, 0), (0, 1)]
+
+def find_route(rows, columns) -> bool:
+  for y in range(rows):
+    line = ""
+    for x in range(columns):
+      if what(x, y, n) == ".":
+        for x_offset, y_offset in directions:
+          x2 = x + x_offset
+          y2 = y + y_offset
+          if x2 > 0 and y2 > 0:
+            if what(x2, y2, n) == ".":
+              G.add_edge((x, y), (x2,y2))
+
+  try:
+    return nx.dijkstra_path_length(G, (1,1), (target_x, target_y))
+  except:
+    return None
+
+rows = target_y
+columns = target_x
+while (first_solution := find_route(rows, columns)) is None:
+  rows += 1
+  columns += 1
+
+print(first_solution)
+print(find_route(first_solution, first_solution))
